afda02564e171b72fdd221b2c73ae2793bf29f4f
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2022-01-28 15:26:51 +0500

    port changed

--- index.js
+++ index.js
@@ -60,7 +60,26 @@ const start = async () => {
         }
 
         // Listening port
-        const port = 3000 || 8080
+        // const port = 3000 || 8080
+        const port = normalizePort(process.env.PORT || '3000');
+        app.set('port', port);
+
+        function normalizePort(val) {
+            const port = parseInt(val, 10);
+
+            if (isNaN(port)) {
+                // named pipe
+                return val;
+            }
+
+            if (port >= 0) {
+                // port number
+                return port;
+            }
+
+            return false;
+        }
+
 
         app.listen(port, () => {
             console.log(`Server watching ${port}...`);
